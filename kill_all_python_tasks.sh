5288f02165b8ca4cc8ca473407413a4a0b061f3a
Thiago F. Costa <thiagofigcosta@google.com>
2023-01-02 07:48:43 -0300

    fix killer

--- kill_all_python_tasks.sh
+++ kill_all_python_tasks.sh
@@ -2,5 +2,5 @@
 
 while true; do
     sleep 3
-    ps -aux | grep python | grep -v grep | awk '{print $2}' | xargs -I {} kill -9 {}
+    ps -aux | grep python | grep -v grep | grep -v kill_all_python_tasks | awk '{print $2}' | xargs -I {} kill -9 {}
 done
